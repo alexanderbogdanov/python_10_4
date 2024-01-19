f8d960dbf46e22686eed4576601181de17cecb9c
alexandrbogdanov <al1029ex@gmail.com>
2024-01-19 13:51:38 +0100

    test 5 implemented

--- test_homework.py
+++ test_homework.py
@@ -1,6 +1,6 @@
 import random
 from math import pi
-from random import randint
+# from random import randint
 
 
 def test_greeting():
@@ -64,8 +64,7 @@ def test_unique_elements():
     Удалите из списка все повторяющиеся элементы
     """
     l = [1, 2, 3, 4, 5, 5, 5, 6, 7, 8, 8, 9, 10, 10]
-    # TODO удалите повторяющиеся элементы
-
+    l = list(set(l))
     assert isinstance(l, list)
     assert len(l) == 10
     assert l == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
